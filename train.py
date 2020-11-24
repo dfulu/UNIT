5fd72e266709b712338abde1bdf5efd641fd24de
James Fulton <djamesfulton@yahoo.co.uk>
2020-11-24 12:03:34 +0000

    add new config

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ import tensorboardX
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='configs/hadgem3_to_cam5_nat-hist.yaml', help='Path to the config file.')
-parser.add_argument('--output_path', type=str, default='.', help="outputs path")
+parser.add_argument('--output_path', type=str, default='/home/dfulu/model_outputs', help="outputs path")
 parser.add_argument("--resume", action="store_true")
 opts = parser.parse_args()
 
