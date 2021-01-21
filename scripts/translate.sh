41b02b3711c4337f9ae574a21800ec91154f7c3f
James Fulton <djamesfulton@yahoo.co.uk>
2021-01-21 12:49:05 +0000

    track created files

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -5,3 +5,5 @@ python translate.py --config ~/model_outputs/outputs/hadgem3_to_cam5_nat-hist-4c
 python translate.py --config ~/model_outputs/outputs/hadgem3_to_cam5_nat-hist-v3/config.yaml --output_zarr /datadrive/cam5/nat_hist_to_hadgem3_v3_zarr --checkpoint ~/model_outputs/outputs/hadgem3_to_cam5_nat-hist-v3/checkpoints/gen_00550000.pt --x2x b2a --seed 237897
 
 python translate.py --config ~/model_outputs/outputs/hadgem3_to_cam5_nat-hist-v5/config.yaml --output_zarr /datadrive/cam5/nat_hist_to_hadgem3_v5_zarr --checkpoint ~/model_outputs/outputs/hadgem3_to_cam5_nat-hist-v5/checkpoints/gen_00069000.pt --x2x b2a --seed 202002111018
+
+python translate.py --config ~/model_outputs/outputs/hadgem3_to_cam5_nat-hist-v5/config.yaml --output_zarr /datadrive/hadgem3/nat_hist_to_hadgem3_v5.1_zarr --checkpoint ~/model_outputs/outputs/hadgem3_to_cam5_nat-hist-v5/checkpoints/gen_00056000.pt --x2x a2b --seed 202015121437
